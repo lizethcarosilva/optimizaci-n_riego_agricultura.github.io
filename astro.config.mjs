866756144090af73d05655bd56ce17985371b765
Lizeth_caro <lizethandreacarosilva@gmail.com>
2025-10-30 00:59:26 -0500

    chore: base de Astro para GitHub Pages y navegaciÃ³n prefix; estilos mejorados en layout y Home

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lizethcarosilva.github.io/optimizaci-n_riego_agricultura.github.io/'
+	site: 'https://lizethcarosilva.github.io/optimizaci-n_riego_agricultura.github.io/',
+	base: '/optimizaci-n_riego_agricultura.github.io/'
 });
